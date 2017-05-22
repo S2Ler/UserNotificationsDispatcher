e4c7daf928e87c3478953134c8aa6af4dcce6485
Alexander Belyavskiy <diejmon@gmail.com>
2017-05-22 17:38:23 +0300

    Add Xcode project

---
+++ Xcode/UserNotificationsDispatcher/UserNotificationsDispatcher.h
@@ -0,0 +1,19 @@
+//
+//  UserNotificationsDispatcher.h
+//  UserNotificationsDispatcher
+//
+//  Created by Aliaksandr Bialiauski on 5/22/17.
+//  Copyright © 2017 Aliaksandr Bialiauski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserNotificationsDispatcher.
+FOUNDATION_EXPORT double UserNotificationsDispatcherVersionNumber;
+
+//! Project version string for UserNotificationsDispatcher.
+FOUNDATION_EXPORT const unsigned char UserNotificationsDispatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserNotificationsDispatcher/PublicHeader.h>
+
+
